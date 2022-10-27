8016ff264bf5d34940a80716b65c6c106a6bdb9f
IvanVolkOFF25 <ivan.s.volkov@vsegda-da.com>
2022-10-28 00:15:02 +0300

    Initial commit

---
+++ src/main/java/Trugolnik.java
@@ -0,0 +1,9 @@
+public class Trugolnik {
+    public static void main(String[] args){
+        int a = 8;
+        int b = 6;
+        int output = (a * b) * 0,5;
+        System.out.println("Площадь треугольника равна: " + output);
+
+    }
+}
